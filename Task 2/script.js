569ffa2a169bc9af12e33085f0ce14f2f068a775
esfaltas <ei.majauskas@gmail.com>
2022-09-29 15:12:26 +0300

    Second task uploaded

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,11 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+document.querySelector('#btn__element').addEventListener('click', plusOne);
+let clicks = 0;
+    function plusOne() {
+        clicks += 1;
+        document.getElementById("btn__state").innerHTML = clicks;
+    }
+    
