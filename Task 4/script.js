c774ef9c3282131c5749be899fb8e7d7fa34f6fa
esfaltas <ei.majauskas@gmail.com>
2022-10-01 14:41:21 +0300

    Fourth task 1/2 uploaded

--- Task 4/script.js
+++ Task 4/script.js
@@ -1,6 +1,6 @@
 /* ------------------------------ TASK 4 -----------------------------------
 Parašykite JS kodą, vartotojui atėjus į tinklapį kreipsis į cars.json failą
-ir iš atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
+ir iš jo atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
 Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
 nurodomas gamintojas ir jo pagaminti modeliai.
 
@@ -10,3 +10,26 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+const booty = document.querySelector('#output')
+
+const getCars = async () => {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    data.forEach((carBrand) => {
+        const masina = carBrand.brand;
+        const marke = carBrand.models;
+        console.log(marke);
+        console.log(masina);
+        const brand = document.createElement('div');
+        brand.setAttribute('id', 'newoutput');
+        const list = document.createElement('ul');
+        const display = document.createElement('li');
+        list.textContent = masina;
+        display.textContent = marke;
+        list.append(display);
+        booty.append(brand);
+        brand.append(list);   
+    })
+}
+
+getCars();
